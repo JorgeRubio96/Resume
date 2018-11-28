102fed7ce51a80ba74d23aab0ea7ecc79794ec09
JorgeRubio96 <jorge.rubiobarboza96@gmail.com>
2018-11-28 12:01:39 -0600

    wip

--- app/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	var skillBars = $('.skill-bar');
-	$.each(skillBars, function() {
-		percent = $(this).attr('data-percent');
-		$(this).append('<div style="width:' + percent + '%"></div>');
-	});
-});
